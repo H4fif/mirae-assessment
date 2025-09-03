80ab62cc3dd345f134fb1a8e46b13fa788dc5591
Hafif I. <hafif.i@smt-id.com>
2025-09-03 17:20:14 +0700

    🐛 Bugfix login and added suspense loading component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,27 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { SiteHeader } from '@/components/site-header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
-import { Helmet } from 'react-helmet';
 import { useAuthStore } from '@/stores/auth.store';
 import { useNavigate } from 'react-router';
-import { type FC, type PropsWithChildren } from 'react';
+import { Suspense, useEffect, type FC, type PropsWithChildren } from 'react';
+import Loader from '@/components/loader';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   const navigate = useNavigate();
   const { auth } = useAuthStore();
 
-  if (!auth) {
-    navigate('/login');
+  useEffect(() => {
+    if (!auth?.accessToken) {
+      navigate('/login');
+    }
+  }, [auth?.accessToken, navigate]);
+
+  if (!auth?.accessToken) {
+    return <Loader />;
   }
 
   return (
     <>
-      <Helmet title="Dashboard | Mirae Assesment" />
-
       <SidebarProvider
         style={
           {
@@ -32,51 +36,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
         <SidebarInset>
           <SiteHeader />
 
-          {/* <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                <SectionCards />
-
-                <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 px-4 lg:px-6">
-                  <ChartBarInteractive />
-                  <ChartBarStacked />
-                  <ChartLineLabel
-                    title="Client Asset AUM"
-                    description={
-                      <div className="flex justify-end items-center">
-                        <div>
-                          <div className="flex items-center gap-2">
-                            <span className="rounded-full size-4 inline-block bg-[#F4720F]"></span>
-                            Client Asset AUM
-                          </div>
-
-                          <div className="flex items-center gap-2">
-                            <span className="rounded-full size-4 inline-block bg-[#7CB6D3]"></span>
-                            Exchange (MM)
-                          </div>
-                        </div>
-                      </div>
-                    }
-                  />
-
-                  <ChartLineLabel
-                    title="Trading Value"
-                    description={
-                      <div className="flex justify-end items-center">
-                        <div className="flex items-center gap-2">
-                          <span className="rounded-full size-4 inline-block bg-[#F4720F]"></span>
-                          Domestic (SMM)
-                        </div>
-                      </div>
-                    }
-                  />
-                </div>
-
-                <DataTable data={data} />
-              </div>
-            </div>
-          </div> */}
-          {children}
+          <Suspense fallback={<Loader />}>{children}</Suspense>
         </SidebarInset>
       </SidebarProvider>
     </>
