0343b03bbf53e88b7cde03d4d11d40334cca22aa
Hafif I. <hafif.i@smt-id.com>
2025-09-03 09:31:41 +0700

    🚨 eslint and update UI

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -33,8 +33,8 @@ import {
 
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "Admin",
+    email: "admin@example.com",
     avatar: "/avatars/shadcn.jpg",
   },
   navMain: [
@@ -160,7 +160,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold">Mirae Assesment</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
