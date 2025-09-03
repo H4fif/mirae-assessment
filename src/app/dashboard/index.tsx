a24c3a7d5a390ba7ec2e8d4b0f12bca66e1a1f82
Hafif I. <hafif.i@smt-id.com>
2025-09-03 09:31:58 +0700

    💄 Bugfix response charts UI

--- src/app/dashboard/index.tsx
+++ src/app/dashboard/index.tsx
@@ -33,7 +33,7 @@ export default function DashboardPage() {
               <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                 <SectionCards />
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:px-6">
+                <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 px-4 lg:px-6">
                   <ChartBarInteractive />
                   <ChartBarStacked />
                   <ChartLineLabel />
