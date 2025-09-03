670961fdaa9c451bfff6e90e713dab255cbbcbff
Hafif I. <hafif.i@smt-id.com>
2025-09-03 16:39:54 +0700

    ⚡️ Optimize component by implementing code splitting

---
+++ src/components/data-table/drag-handle.tsx
@@ -0,0 +1,23 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { Button } from '../ui/button';
+import { IconGripVertical } from '@tabler/icons-react';
+
+function DragHandle({ id }: { id: number }) {
+  const { attributes, listeners } = useSortable({
+    id,
+  });
+
+  return (
+    <Button
+      {...attributes}
+      {...listeners}
+      variant="ghost"
+      size="icon"
+      className="text-muted-foreground size-7 hover:bg-transparent"
+    >
+      <IconGripVertical className="text-muted-foreground size-3" />
+      <span className="sr-only">Drag to reorder</span>
+    </Button>
+  );
+}
+export default DragHandle;
